fa550d74df3597a3bd262179d4ab1d61997f898a
cgladis <cgladis@student.21-school.ru>
2022-05-31 00:02:54 +0500

    adding random color

--- paint.py
+++ paint.py
@@ -1,15 +1,14 @@
-import kivy
 from kivy.app import App
 from kivy.graphics import Color, Ellipse, Line
 from kivy.uix.widget import Widget
-
-kivy.require('2.1.0')  # replace with your current kivy version !
+from random import random
 
 
 class MyPaintWidget(Widget):
     def on_touch_down(self, touch):
+        color = (random(), random(), random())
         with self.canvas:
-            Color(1, 1, 0)
+            Color(*color)
             d = 30.
             Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
             touch.ud['line'] = Line(points=(touch.x, touch.y))
