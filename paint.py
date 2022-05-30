e17e3825996ed93037a3cdd6201760c7b19e092f
cgladis <cgladis@student.21-school.ru>
2022-05-30 23:59:19 +0500

    adding line drawing

--- paint.py
+++ paint.py
@@ -1,6 +1,6 @@
 import kivy
 from kivy.app import App
-from kivy.graphics import Color, Ellipse
+from kivy.graphics import Color, Ellipse, Line
 from kivy.uix.widget import Widget
 
 kivy.require('2.1.0')  # replace with your current kivy version !
@@ -12,6 +12,10 @@ class MyPaintWidget(Widget):
             Color(1, 1, 0)
             d = 30.
             Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+            touch.ud['line'] = Line(points=(touch.x, touch.y))
+
+    def on_touch_move(self, touch):
+        touch.ud['line'].points += [touch.x, touch.y]
 
 
 class MyPaintApp(App):
