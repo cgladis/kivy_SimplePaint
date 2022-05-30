6eadc0be248b5242fa723db5e26a8c2bcff65ad5
cgladis <cgladis@student.21-school.ru>
2022-05-30 23:50:55 +0500

    small yellow circle drawn where you touched

--- paint.py
+++ paint.py
@@ -1,11 +1,17 @@
+import kivy
 from kivy.app import App
+from kivy.graphics import Color, Ellipse
 from kivy.uix.widget import Widget
 
 kivy.require('2.1.0')  # replace with your current kivy version !
 
 
 class MyPaintWidget(Widget):
-    pass
+    def on_touch_down(self, touch):
+        with self.canvas:
+            Color(1, 1, 0)
+            d = 30.
+            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
 
 
 class MyPaintApp(App):
